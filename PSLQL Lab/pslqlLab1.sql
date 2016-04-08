a475341e2295261f2fb131ded03ba729ac6809b8
Anna Paula Maule <annamaule@usp.br>
2016-04-08 11:52:08 -0700

    exception added to the script

--- PSLQL Lab/pslqlLab1.sql
+++ PSLQL Lab/pslqlLab1.sql
@@ -1,7 +1,13 @@
 create or replace porcedure plsql_lab(
 		emp_id in employees.employee_id%type, 
 		ref_cursor out sys_refcursor)  -- is not a direct assigment , just what is coming out 
-		
+						-- ref_cursor is how we select things from the server side 
+
+--  example od bind variable :  var ret_cur refcursor
+--  do not confuse bind variable with substitution variable 
+ 
+
+						
 as 	
 		cursor try_param(dp_id number) is 
 			select last_name from employees where department_id = dp_id; 
@@ -9,7 +15,7 @@ as
 		dep_id			departments.department_id%type;
 		dep_name		departments.department_name%type;
 
-begin 
+begin 			-- this is all on the server side 
 		select last_name, department_id, department_name, 
 				into emp_name, dep_name, dep_id
 				from employees
@@ -25,7 +31,9 @@ begin
 				
 				open ref_cursor for select last_nam from employees 
 					where department_id = dep_id; 
-
+exception 
+		when no_data_found then dbms_output.put_line('The employee does not exist');
+		when others then dbms_output.put_line("Oopss"); 
 	null; 
 	
 end; 
@@ -33,6 +41,6 @@ end;
 sho err -- it will show the errors 
 
 -- save the file as " .pro"
--- it does not matter the extension tyoe that is saved 
+-- it does not matter the extension type that the file is saved 
 
 
